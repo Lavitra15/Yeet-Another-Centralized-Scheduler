62a1bd6e5cfa203525bb28184ed84778ac00e963
Lavitra Madan <53050849+Lavitra15@users.noreply.github.com>
2020-12-02 00:46:50 +0530

    major fixes

--- master.py
+++ master.py
@@ -1,196 +1,136 @@
-#Making the necessary imports 
 import sys 
 import os 
-import operator 
+import operator
 import random
 import socket
-from thread import *
-import threading 
-import csv
-import math 
-import seaborn  
-from datetime import sleep,time #should this be just time module ?
+import threading
+import datetime
+import json
+from time import sleep
 
-#path to config - 
-config_path = sys.argv[1]
-# if it is needed to take cli arguments 
-# sched_algo = sys.argv[2]
+workerLock = threading.Lock()
+jobLock = threading.Lock()
 
-#Uncomment the scheduling algorithm to see its implementation 
-
-#Implementation of the scheduling algorithms
- 
-#Creating datastructures  
-worker_state = dict()
-job_state = dict()
-map_tasks = list()
-reduce_tasks = list()
-
-#Assigning a lock for each 
-worker_state_lock = threading.Lock()
-job_state_lock = threading.Lock()
-map_tasks_lock = threading.Lock()
-reduce_tasks_lock = threading.Lock()
-
-
-#1. Random Scheduling 
-def random_scheduling():
+def RANDOM(task,workers,algo):
     while True:
-        workers_state_lock.acquire()
-        choice = random.randint(1,3) #can choose a random number based on keys as well 
-        free_slots = worker_state[choice]['slots'] - worker_state[choice]['occupied slots']
-        #can include a print statement here to check for choice 
-        if free_slots > 0:
-            print(choice)
-            #if slots in worker are free - increment occupied and release lock 
-            worker_state[choice]['occupied_slots'] += 1
-            worker_state_lock.release()
+        workerLock.acquire()
+        choice = random.randint(0,len(workers)-1) #can choose a random number based on keys as well 
+        if workers[choice]['freeSlots']>0:
+            workers[choice]['freeSlots']-=1
+            mouth=socket.socket()
+            mouth.connect(('Localhost'),workers[choice]['port'])
+            msg=json.dumps(task).encode()
+            mouth.send(msg)
+        workerLock.release()
 
-        worker_state_lock.release()
-        sleep(0.005)
+def RR(task,workers,algo):
+    i=0
+    workerLock.acquire()
+    while True:
+        if workers[i]['freeSlots']>0:
+            workers[i]['freeSlots']-=1
+            mouth=socket.socket()
+            mouth.connect(('Localhost'),workers[i]['port'])
+            msg=json.dumps(task).encode()
+            mouth.send(msg)
+            workerLock.release()
+            break
+        i=(i+1)%len(workers)
+        if i==0:
+            sleep(1)
 
+def LL(task,workers,algo):
+    workerLock.acquire()
+    while True:
+        min_i=0
+        min_free=workers[0]['freeSlots']
+        for i in range(1,len(workers)):
+            if workers[i]['freeSlots']>min_free:
+                min_free=workers[i]['freeSlots']
+                min_i=i
+        if min_free:
+            workers[min_i]['freeSlots']-=1
+            mouth=socket.socket()
+            mouth.connect(('Localhost'),workers[min_i]['port'])
+            msg=json.dumps(task).encode()
+            mouth.send(msg)
+            workerLock.release()
+            break
+        sleep(1)
 
-'''  
-#2. Round Robin 
-def round_robin():
+def yeetacs(jobs, algo, workers):
+    algorithm={'RANDOM':RANDOM, 'RR':RR,'LL':LL}
+    while True:
+        for job in jobs:
+            if len(jobs[job]['mapTasks'])>0:
+                task={'jobID':job,'taskID':jobs[job]['mapTasks'][0]['task_id'],'time':jobs[job]['mapTasks'][0]['duration']}
+                algorithm[algo](task,workers,algo)
+            else:
+                task={'jobID':job,'taskID':jobs[job]['reduceTasks'][0]['task_id'],'time':jobs[job]['reduceTasks'][0]['duration']}
+                algorithm[algo](task,workers,algo)
 
-#3. Least-Loaded 
-def least_loaded():
-'''
-
-#the actual scheduler 
-def yeetacs():
-
-    print("ahahaha") #debug 
-
-    #to decide on sched algo - can be taken from the command line 
-    if sched_algo == "Random":
-        worker_id = random_scheduling()
-    elif sched_algo == "RR":
-        worker_id = round_robin()
-    elif sched_algo == "LL":
-        worker_id == least_loaded()
-    else:
-        print("Enter correct arguments else Big rip for worker")
-
-    #need to acquire and release locks and also connect socket to localhost 
-    
-
-
-    
-#Assigning the task to the scheduler - 
-
-
-
-
-#Listen to job requests
-def getJobRequests():
-    host = ""
-    port = 5000 #listens on port 5000 for jobs and 5001 for workers 
-    job_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #Establishing a TCP connection for job socket 
-    job_socket.bind(host,port)
-    job_socket.listen(500) #Can change this param if needed - to listen 
-
+def getJobRequests(jobs):
+    ear = socket.socket() #Establishing a TCP connection for job socket 
+    ear.bind(('',5000))
+    ear.listen(500) #Can change this param if needed - to listen 
     while(True):
-        connection,address = job_socket.accept()
-        received = connection.rcv(2048)
-        #print arrival time using datetime.now()
-        job = json.load(received)
-
-        #acquire locks 
-        map_tasks_lock.acquire()
-        job_state_lock.acquire()
-
-        job_id = job['job_id']
-        job_state[job_id] = dict()
-        job_state[job_id]["arrival_time"] = arrival_time
-        job_state[job_id]["map tasks"] = job_state["map tasks"]
-        job_state[job_id]["map completed"] = False
-        job_state[job_id]["total completed"] = False 
-
-        for i in range(len(job["map_tasks"])):
-            job_state[job_id]["map_tasks"][i]['job_id'] = job_id
-        
-        job_state[job_id]["reduce_tasks"] = job["reduce_tasks"]
-
-        for i in range(len(job["reduce_tasks"])):
-            job_state[job_id]["reduce_tasks"][i]['job_id'] = job_id
-
-        job_state[job_id]["map_tasks_completed"] = list()
-        job_state[job_id]["reduce_tasks_completed"] = list()
-
-        map_tasks.extend(job_state[job_id]["map_tasks"])
-
-        #release locks 
-        job_state_lock.release()
-        map_tasks_lock.release()
-
+        connection,address = ear.accept()
+        requests = connection.recv(65536).decode()
+        arrival_time=datetime.datetime.now()
+        job = json.load(requests)
+        jobLock.acquire()
+        workerLock.acquire()
+        log=open('masterlog.txt','a')
+        log.write(arrival_time+'JobArrived'+job['job_id'])
+        log.close()
+        workerLock.release()
+        jobID = job['job_id']
+        jobs[jobID] = {'mapTasks':job["map tasks"], "reduceTasks":job["reduce_tasks"]}
+        jobLock.release()
         connection.close()
 
+def getWorkerMessage(workers):
+    ear2=socket.socket()
+    ear2.bind(('',5001))
+    ear2.listen(len(workers))
+    while True:
+        connection,address=ear2.accept()
+        msg=connection.recv().decode()
+        removedTask=json.loads(msg)
+        workerLock.acquire()
+        workers[removedTask['workerID']]['freeSlots']+=1
+        workerLock.release()
+        jobLock.acquire()
+        if 'M' in removedTask['taskID']:
+            jobs[removedTask['workerID']]['mapTasks'].pop([int(removedTask['taskID'].split('M')[-1])])
+        else:
+            jobs[removedTask['workerID']]['reduceTasks'].pop([int(removedTask['taskID'].split('R')[-1])])
+            if len(jobs[removedTask['workerID']]['reduceTasks'])==0:
+                workerLock.acquire()
+                log=open('masterlog.txt','a')
+                log.write('JobFinished'+jobs['job_id'])
+                log.close()
+                workerLock.release()
+                jobs.pop(removedTask['jobID'])
+        jobLock.release()
+        connection.close()
 
-#Listen to worker requests  
-def getWorkerRequests():
-
-
-
-
-
-
-
-
-
-
-
-#Main function 
-
-def main():
-    #make a separate directory to log file 
-    try:
-        os.mkdir(log)
-    except:
-        pass #log directory already exists from a previous test run 
-
-    #Open the log file on master to write on it 
-    f = open('log/master.txt','w')
-    f.close() #close the file after writing logs 
-    config = open('config_path')
-    json.load(config)
-    #print(config) - uncomment if needed
-
-    #creating a global variable for worker slots and filling it using dict as the implemented data stucture
-    global workerSlots
-    
-    #creating a json object for workers
-    workers = config['workers']
-    for worker in workers:
-        worker_state = worker['worker_id']
-    
-    for state in worker_state:
-        worker_state[state]['occupied_slots'] = 0
-    
-    #A lock to be released here for worker?
-
-    #debugging - check if number of slots are proper
-    print(workerSlots)
-
-    #getting the actual worker threads and assigning them targets to run the jobs on 
-    thread1 = threading.thread(target = getJobRequests)
-    thread2 = threading.thread(target = getWorkerRequests)
-    thread3 = threading.thread(target = yeetacs)
-
-    #Starting the worker threads for communication with the master
-    thread1.start()
-    thread2.start()
-    thread3.start()
-
-    #Joining the above memtioned thread 
-    thread1.join()
-    thread2.join()
-    thread3.join()
-
-    #Pls work
-    print("It works!")
-
-#Running the main function 
-if __name__ == '__main__':
-    main()
+conf_file=sys.argv[1]
+algo=sys.argv[2]
+config = open(conf_file)
+config=json.load(config)
+workers = config['workers']
+for worker in workers:
+    worker['freeSlots'] = worker['slots']
+log=open('masterlog.txt','w')
+log.close()
+jobs={}
+thread1 = threading.Thread(target = getJobRequests, args=(jobs,))
+thread2 = threading.Thread(target = getWorkerMessage, args=(workers,))
+thread3 = threading.Thread(target = yeetacs, args=(jobs, algo, workers))
+thread1.start()
+thread2.start()
+thread3.start()
+thread1.join()
+thread2.join()
+thread3.join()
